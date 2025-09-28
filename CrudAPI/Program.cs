10a4063fb2fcb2b47c5437f60e655527bcef7c44
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-09-27 23:32:44 -0600

    Configurando BD en SQL Server

--- CrudAPI/Program.cs
+++ CrudAPI/Program.cs
@@ -1,3 +1,6 @@
+using CrudAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSql"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
